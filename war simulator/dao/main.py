ec642bb3e9a4926c5ef6ab122bfcfde6cafbdae6
I10kram <114987248+I10kram@users.noreply.github.com>
2022-12-29 22:40:42 +0100

    Add files via upload

---
+++ war simulator/dao/main.py
@@ -0,0 +1,8 @@
+class sum:
+    def __init__(self) -> None:
+        pass
+    def sum1( x:int , y:int , z:int  ):
+        return x+y+z
+
+f=sum.sum1(459867546584215485618945516551615619451,54689,569875)
+print(f)
